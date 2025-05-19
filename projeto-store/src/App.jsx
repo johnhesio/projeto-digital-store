b6cce85c772801ab861c9a9a87e6eba3dc2c0eb0
johnhesio <john_hesio@hotmail.com>
2025-05-19 16:06:38 -0300

    add images

--- projeto-store/src/App.jsx
+++ projeto-store/src/App.jsx
@@ -1,16 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <h1 class="text-3xl font-bold underline bg-blue-500">Hello world!</h1>
-    </>
+    <></>
   );
-}
+};
 
 export default App;
---
+++ projeto-store/src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <>
+      <header className="w-full bg-blue-200 shadow-sm">
+        <h1>teste</h1>
+        <img src="" alt="" />
+      </header>
+    </>
+  );
+};
+
+export default Header;
